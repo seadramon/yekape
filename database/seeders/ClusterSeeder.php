0018a2744e232fe809ca01b26205bfe6b7ce15d1
allenwalker00 <rahmatdhidayat@gmail.com>
2023-12-06 09:55:51 +0700

    add cluster

---
+++ database/seeders/ClusterSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TanahMentah;
+use App\Models\Cluster;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClusterSeeder extends Cluster
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cluster = json_decode(file_get_contents(public_path('migrations/cluster.json')))->cluster;
+        foreach ($cluster as $item) {
+            $comm = Cluster::firstOrNew([
+                'tanah_mentah_id' => $cluster[$item->mentah_id]->first()->id,
+            ]);
+            $comm->nama = $item->nama;
+            $comm->lokasi = $item->lokasi;
+            $comm->save();
+        }
+    }
+}
